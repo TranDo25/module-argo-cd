d128b82520d47162233012a7567923c74dd339ba
Trần Xuân Độ <73243952+TranDo25@users.noreply.github.com>
2025-08-31 16:18:37 +0700

    sua lai module cho dung

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+    }
+  }
+}
+
+# Cấu hình Kubernetes provider cho EKS
 provider "kubernetes" {
   host                   = var.kubernetes_cluster_endpoint
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -8,17 +22,20 @@ provider "kubernetes" {
   }
 }
 
-provider "helm" {
-}
+# Helm provider KHÔNG cần block kubernetes nữa, chỉ cần để trống
+provider "helm" {}
 
-resource "kubernetes_namespace" "example" {
+# Tạo namespace ArgoCD
+resource "kubernetes_namespace" "argo" {
   metadata {
     name = "argo"
   }
 }
+
+# Cài đặt ArgoCD bằng Helm
 resource "helm_release" "argocd" {
   name       = "msur"
-  chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argo"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argo.metadata[0].name
 }
