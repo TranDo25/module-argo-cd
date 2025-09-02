80009dfd2b6a0cf208d6437af677cab70d5fbf5b
Trần Xuân Độ <73243952+TranDo25@users.noreply.github.com>
2025-09-02 19:42:11 +0700

    update new main

--- main.tf
+++ main.tf
@@ -1,41 +1,26 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.29"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-    }
-  }
-}
 
-# Cấu hình Kubernetes provider cho EKS
 provider "kubernetes" {
-  host                   = var.kubernetes_cluster_endpoint
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
   }
 }
+provider "helm" {
+}
 
-# Helm provider KHÔNG cần block kubernetes nữa, chỉ cần để trống
-provider "helm" {}
-
-# Tạo namespace ArgoCD
-resource "kubernetes_namespace" "argo" {
+resource "kubernetes_namespace" "example" {
   metadata {
     name = "argo"
   }
 }
-
-# Cài đặt ArgoCD bằng Helm
 resource "helm_release" "argocd" {
   name       = "msur"
-  repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argo.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argo"
 }
